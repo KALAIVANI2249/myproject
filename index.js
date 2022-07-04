c9a447097c229d7e4cd4aed54479d5bdb64b570d
KALAIVANI2249 <108512203+KALAIVANI2249@users.noreply.github.com>
2022-07-04 18:08:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var express=require("express");
+var app=express();
+var path=require("path");
+app.use(express.static(path.join(__dirname, "public/styles/")));
+app.use(express.static(path.join(__dirname, "public/scripts/")));
+app.use(express.static(path.join(__dirname, "public/img/")));
+app.get("/",function(request,response){
+    response.send("Node JS is working");
+})
+
+app.get("/greet/:name",function(request,response){
+    response.send("Hello!!!"+request.params.name);
+})
+
+app.get("/home",function(request,response){
+    response.sendFile(path.join(__dirname,"public/index.html"));
+    
+})
+
+app.listen("3000",function(){
+    console.log("server is started on port number 3000")
+})
